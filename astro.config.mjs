c5866ec28b5363a3bd7fabb7ced1c8736eb1f040
Justinas Kavoliūnas <justinaskav@gmail.com>
2023-09-09 01:30:20 +0300

    Remove unneeded stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -1,7 +1,9 @@
 import type { App } from 'vue';
-import PrimeVue from "primevue/config";
-import Tailwind from "primevue/passthrough/tailwind";
+// import PrimeVue from "primevue/config";
+// import Tailwind from "primevue/passthrough/tailwind";
+
+// import { type PrimeVuePTOptions } from 'primevue/config';
 
 export default (app: App) => {
-    app.use(PrimeVue, { unstyled: true, pt: Tailwind });
+    // app.use(PrimeVue, { unstyled: true, pt: Tailwind_PT });
   };
